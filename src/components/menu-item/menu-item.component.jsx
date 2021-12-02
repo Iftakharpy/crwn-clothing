bce7f2974591b4055ee2977438647e08de585f13
Iftakhar Husan <iftakharn19@gmail.com>
2021-12-02 20:56:00 +0600

    Change default font add styles for hover state

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,12 +3,13 @@ import "./menu-item.styles.scss";
 
 function MenuItem({ title, imageUrl, size }) {
   return (
-    <section
-      className={`${size} menu-item`}
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    >
+    <section className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">Shop Now</span>
