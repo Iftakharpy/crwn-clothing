25cbccf7dff14d5f0ca02ae4a784e015dd535238
Iftakhar Husan <iftakharn19@gmail.com>
2021-12-05 05:48:06 +0600

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
-import "./App.css";
+// React
+import React, { Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
+
+// Custom components
 import HomePage from "./pages/homepage/homepage.component";
+import Shop from "./pages/shop/shop.component";
+
+// Styles
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="crwn-clothing" element={<Fragment>{<Outlet />}</Fragment>}>
+          <Route path="" element={<HomePage />} />
+          <Route path="shop" element={<Shop />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
