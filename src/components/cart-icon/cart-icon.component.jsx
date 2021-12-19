2d72960d081fa2ea7dd697bfd42306170f21d2c2
Iftakhar Husan <iftakharn19@gmail.com>
2021-12-20 03:22:52 +0600

    Add cart icon component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 
+import { useSelector, useDispatch } from "react-redux";
+import { toggleItemsVisibility } from "../../redux/features/cart/cartSlice";
+
 import { ReactComponent as ShoppingIcon } from "../../assets/11.1 shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
 
 const ShoppingCart = () => {
+  const dispatch = useDispatch();
+  const itemsCount = useSelector((state) => state.cart.items.length);
   return (
-    <div className="cart-icon">
+    <div
+      className="cart-icon"
+      onClick={() => dispatch(toggleItemsVisibility())}
+    >
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemsCount}</span>
     </div>
   );
 };
