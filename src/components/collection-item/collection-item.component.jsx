e33ef09b4ed2dcc472745ac9b2a589a3b9eafb9c
Iftakhar Husan <iftakharn19@gmail.com>
2021-12-20 05:09:11 +0600

    Handle add to cart functionality for products

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,12 +1,18 @@
 // react
 import React from "react";
 
+import { useSelector, useDispatch } from "react-redux";
+import { addItem, increaseCount } from "../../redux/features/cart/cartSlice";
+
 import CustomButton from "../custom-button/custom-button.component";
 
 // style
 import "./collection-item.style.scss";
 
-export default function CollectionItem({ id, name, price, imageUrl }) {
+const CollectionItem = (product) => {
+  let cartItems = useSelector((state) => state.cart.items);
+  const { name, price, imageUrl } = product;
+  const dispatch = useDispatch();
   return (
     <div className="collection-item">
       <div
@@ -19,7 +25,25 @@ export default function CollectionItem({ id, name, price, imageUrl }) {
         <span className="name">{name}</span>
         <span className="price">${price}</span>
       </div>
-      <CustomButton inverted>Add to cart</CustomButton>
+      <CustomButton
+        onClick={() => {
+          let productInCart = false;
+          cartItems.forEach((element) => {
+            if (element.product.id === product.id) {
+              productInCart = true;
+              return;
+            }
+          });
+          productInCart
+            ? dispatch(increaseCount(product))
+            : dispatch(addItem(product));
+        }}
+        inverted
+      >
+        Add to cart
+      </CustomButton>
     </div>
   );
-}
+};
+
+export default CollectionItem;
