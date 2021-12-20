cd66fce8447f32291133a5d7d6e3c8fa78e39849
Iftakhar Husan <iftakharn19@gmail.com>
2021-12-20 14:14:03 +0600

    Add few useful features as comment and ignore actions recommended by @reduxjs/tookit docs

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
-import { persistReducer } from "redux-persist";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+// Local Storage
 import storage from "redux-persist/lib/storage";
+// // Session Storage
+// import storageSession from "redux-persist/lib/storage/session";
+
+// // State Reconciler
+// import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
+// // autoMergeLevel1 (default)
+// import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 
 import logger from "redux-logger";
 
@@ -15,14 +31,21 @@ const reducers = combineReducers({
 
 const persistConfig = {
   key: "root",
-  storage: storage,
+  version: 1,
+  storage,
   blacklist: ["user"],
   // whitelist: ["cart"],
+  // stateReconciler: hardSet,
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
 export default configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger]),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat([logger]),
 });
