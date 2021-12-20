d01e0aac86715644280dc61c9190d1daa531b61a
Iftakhar Husan <iftakharn19@gmail.com>
2021-12-20 07:20:30 +0600

    Use nanoid to create unique keys

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { useSelector } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
 
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
@@ -9,12 +10,11 @@ import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
   let cartItems = useSelector((state) => state.cart.items);
-  console.log(cartItems);
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
         {cartItems.map((item) => (
-          <CartItem key={item.id} item={item} />
+          <CartItem key={nanoid(10)} item={item} />
         ))}
       </div>
       <CustomButton>Go To Checkout</CustomButton>
