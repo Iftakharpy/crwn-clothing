c21a30cf42a43cb77c1c97bdb76a8c7ec5c59138
Iftakhar Husan <iftakharn19@gmail.com>
2021-12-20 11:15:17 +0600

    Finish checkout layout

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect } from "react";
 
+import { useSelector } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+
 import "./checkout.styles.scss";
 
 const Checkout = (props) => {
@@ -11,8 +16,41 @@ const Checkout = (props) => {
       if (props?.defaultPageTitle) document.title = props?.defaultPageTitle;
     };
   });
+  const { items } = useSelector((state) => state.cart);
+  const total = items.reduce(
+    (previousTotal, item) => previousTotal + item.product.price * item.quantity,
+    0
+  );
 
-  return <main className="checkout">Checkout Page</main>;
+  return (
+    <main className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+      <div className="checkout-items">
+        {items.map((item) => (
+          <CheckoutItem key={nanoid(5)} item={item} />
+        ))}
+      </div>
+      <div className="total">
+        <span>Total: ${total}</span>
+      </div>
+    </main>
+  );
 };
 
 export default Checkout;
